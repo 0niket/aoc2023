4db6d775f76d6e32ba69e7637e387c87fa4d6f09
Aniket Hendre <hendre.ani@gmail.com>
2023-12-06 00:16:51 +0530

    Parse data for day 2 challenge

---
+++ src/aoc2023/day2.clj
@@ -0,0 +1,37 @@
+(ns aoc2023.day2
+  (:require [clojure.string :as cs]))
+
+(defn game-number
+  [s]
+  (->> s
+       (re-seq #"\d+")
+       first
+       Integer/parseInt))
+
+(defn subrounds
+  [s]
+  (as-> s s
+    (cs/split s #";")
+    (map #(cs/split % #",") s)
+    (map (fn [xs]
+           (apply
+            hash-map
+            (mapcat (fn [ys]
+                     (reverse (cs/split (cs/trim ys) #" ")))
+                   xs)))
+         s)))
+
+(defn line->game-ds
+  [line]
+  (let [game-number-subrounds-split (cs/split line #":")
+        game-number (game-number (first game-number-subrounds-split))
+        subrounds (subrounds (second game-number-subrounds-split))]
+    {:game-id game-number
+     :subrounds subrounds}))
+
+(defn solve
+  []
+  (->> "resources/day2input.txt"
+       slurp
+       cs/split-lines
+       (map line->game-ds)))
